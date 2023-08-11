3dd117da6c13df34f873be4ac8edaba5ee7d7a53
jaychouzzz <f18326186224@gmail.com>
2023-08-11 11:24:55 +0800

    🐞 fix: 更改证书路径

--- settings/config.py
+++ settings/config.py
@@ -4,7 +4,7 @@ ENV = 'PROD'
 # tg配置
 BOT_TOKEN = '6682477712:AAEoEBftr_qvA8Q7EO6QLebhiERSDc-58Fk'
 CHAT_ID = 5913565300
-WEBHOOK_HOST = 'bot.cinima.asia'
+WEBHOOK_HOST = 'www.cinima.asia'
 WEBHOOK_PORT = 88  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 WEBHOOK_SSL_CERT = '/root/code/docker/dockerfile_work/xray/cert/cert.pem'  # Path to the ssl certificate
