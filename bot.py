b5f99064ff4b76060dd0d696d352bcb17d0f2c79
jaychouzzz <f18326186224@gmail.com>
2023-08-22 01:49:14 +0800

    ✨ feat: 开发完毕

--- bot.py
+++ bot.py
@@ -132,7 +132,7 @@ def draw_lottery(message):
     pass
 
 # 每月7号
-@scheduler.task('cron', id='get_traffic_packet', month='*', day='7', hour='9', minute='0', second='0')
+@scheduler.task('cron', id='get_traffic_packet', month='*', day='22', hour='1', minute='51', second='0')
 def get_traffic_packet():
     """自动领取流量包
     """    
@@ -241,12 +241,13 @@ if __name__ == '__main__':
         def robots():
             return "User-agent: *\nDisallow: /", 200
         
-        scheduler.init_app(app)
-        scheduler.start()
         # Start flask server
         app.run(host=config.WEBHOOK_LISTEN,
                 port=config.WEBHOOK_PORT,
                 ssl_context=(config.WEBHOOK_SSL_CERT, config.WEBHOOK_SSL_PRIV),
                 debug=False)
+        
+        scheduler.init_app(app)
+        scheduler.start()
             
         
