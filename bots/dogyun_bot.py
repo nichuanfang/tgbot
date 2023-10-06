e0e5ba7b9ea0ee218a4b954834fbb014e5704cc5
jaychouzzz <f18326186224@gmail.com>
2023-10-06 23:05:14 +0800

    🐞 fix: 抽奖通知报错

--- bots/dogyun_bot.py
+++ bots/dogyun_bot.py
@@ -12,23 +12,26 @@ import subprocess
 
 logger = telebot.logger
 
-bot = telebot.TeleBot(dogyun_config['BOT_TOKEN'],threaded=False)
+bot = telebot.TeleBot(dogyun_config['BOT_TOKEN'], threaded=False)
 
 
 # 连接方法
-def ssh_connect( _host,_port,_username, _password ):
+def ssh_connect(_host, _port, _username, _password):
     try:
         _ssh_fd = paramiko.SSHClient()
-        _ssh_fd.set_missing_host_key_policy( paramiko.AutoAddPolicy() )
-        _ssh_fd.connect( _host, port=_port ,username = _username, password = _password )
+        _ssh_fd.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        _ssh_fd.connect(_host, port=_port, username=_username,
+                        password=_password)
     except Exception as e:
-        print( 'ssh %s@%s: %s' % (_username, _host, e) )
+        print('ssh %s@%s: %s' % (_username, _host, e))
         exit()
     return _ssh_fd
 
 # 运行命令
-def ssh_exec_cmd( _ssh_fd, _cmd):
-    stdin, stdout, stderr =  _ssh_fd.exec_command( _cmd )
+
+
+def ssh_exec_cmd(_ssh_fd, _cmd):
+    stdin, stdout, stderr = _ssh_fd.exec_command(_cmd)
     while not stdout.channel.exit_status_ready():
         result = stdout.readline()
         if stdout.channel.exit_status_ready():
@@ -37,9 +40,11 @@ def ssh_exec_cmd( _ssh_fd, _cmd):
             return
 
 # 关闭SSH
-def ssh_close( _ssh_fd ):
+
+
+def ssh_close(_ssh_fd):
     _ssh_fd.close()
-    
+
 
 @bot.message_handler(commands=['server_info'])
 def get_server_status(message):
@@ -47,14 +52,14 @@ def get_server_status(message):
 
     Returns:
         _type_: _description_
-    """    
+    """
     url = f'https://api.dogyun.com/cvm/server/{dogyun_config["DOGYUN_SERVER_ID"]}'
     headers = {
         'API-KEY': dogyun_config['DOGYUN_API_KEY']
     }
     # GET请求
     try:
-        response = requests.get(url, headers=headers,verify=True)
+        response = requests.get(url, headers=headers, verify=True)
     except Exception as e:
         bot.reply_to(message, f'获取服务器状态失败: {e.args[0]}')
         return
@@ -67,12 +72,14 @@ def get_server_status(message):
         health = server_info['health']
         ip = server_info['ipv4']
         # cpu
-        cpu = round(health['cpu']/health['maxcpu']*100,2)
+        cpu = round(health['cpu']/health['maxcpu']*100, 2)
         # 内存
-        memory = round(health['mem']/health['maxmem']*100,2)
-        bot.reply_to(message, f'IP:  {ip}\n状态:  {data["data"]["status"]}\nCPU:  {cpu}%\n内存:  {memory}%')
+        memory = round(health['mem']/health['maxmem']*100, 2)
+        bot.reply_to(
+            message, f'IP:  {ip}\n状态:  {data["data"]["status"]}\nCPU:  {cpu}%\n内存:  {memory}%')
     else:
         bot.reply_to(message, '获取服务器状态失败')
+
 
 @bot.message_handler(commands=['traffic_info'])
 def send_traffic_info(message):
@@ -80,14 +87,14 @@ def send_traffic_info(message):
 
     Args:
         message (_type_): _description_
-    """   
+    """
     url = f'https://api.dogyun.com/cvm/server/{dogyun_config["DOGYUN_SERVER_ID"]}/traffic'
     headers = {
         'API-KEY': dogyun_config['DOGYUN_API_KEY']
     }
     try:
         # GET请求
-        response = requests.get(url, headers=headers,verify=True)
+        response = requests.get(url, headers=headers, verify=True)
         # 获取返回的json数据
         data = response.json()
     except Exception as e:
@@ -96,16 +103,18 @@ def send_traffic_info(message):
     # 获取流量信息
     traffic_info = data['data'][-1]
     # 被动流入
-    inputIn = round(traffic_info['inputIn']/1000/1000,2)
+    inputIn = round(traffic_info['inputIn']/1000/1000, 2)
     # 被动流出
-    inputOut = round(traffic_info['inputOut']/1000/1000,2)
+    inputOut = round(traffic_info['inputOut']/1000/1000, 2)
     # 主动流入
-    outputIn = round(traffic_info['outputIn']/1000/1000,2)
+    outputIn = round(traffic_info['outputIn']/1000/1000, 2)
     # 主动流出
-    outputOut = round(traffic_info['outputOut']/1000/1000,2)
+    outputOut = round(traffic_info['outputOut']/1000/1000, 2)
     # 总计(保留两位小数)
-    total = round((traffic_info['inputIn'] + traffic_info['inputOut'] + traffic_info['outputIn'] + traffic_info['outputOut'])/1000/1000/1000,2)
-    bot.reply_to(message, f'总计:{total}GB\n\n被动流入:{inputIn}MB\n被动流出:{inputOut}MB\n主动流入:{outputIn}MB\n主动流出:{outputOut}MB')
+    total = round((traffic_info['inputIn'] + traffic_info['inputOut'] +
+                  traffic_info['outputIn'] + traffic_info['outputOut'])/1000/1000/1000, 2)
+    bot.reply_to(
+        message, f'总计:{total}GB\n\n被动流入:{inputIn}MB\n被动流出:{inputOut}MB\n主动流入:{outputIn}MB\n主动流出:{outputOut}MB')
 
 
 @bot.message_handler(commands=['receive_monthly_benefits'])
@@ -114,7 +123,7 @@ def receive_monthly_benefits(message):
 
     Args:
         message (_type_): _description_
-    """ 
+    """
     url = f'https://cvm.dogyun.com/traffic/package/level'
     headers = {
         'X-Csrf-Token': dogyun_config['DOGYUN_CSRF_TOKEN'],
@@ -124,10 +133,11 @@ def receive_monthly_benefits(message):
     }
     try:
         # 发送post请求
-        response = requests.post(url, headers=headers,verify=True)
+        response = requests.post(url, headers=headers, verify=True)
         if response.url == 'https://account.dogyun.com/login':
             # tg通知dogyun cookie已过期
-            bot.send_message(dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
+            bot.send_message(
+                dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
             return
         data = response.json()
     except Exception as e:
@@ -136,7 +146,7 @@ def receive_monthly_benefits(message):
     # 获取领取结果
     result = data['message']
     bot.reply_to(message, result)
-    
+
 
 @bot.message_handler(commands=['draw_lottery'])
 def draw_lottery(message):
@@ -144,7 +154,7 @@ def draw_lottery(message):
 
     Args:
         message (_type_): _description_
-    """    
+    """
     url = f'https://console.dogyun.com/turntable/lottery'
     headers = {
         'X-Csrf-Token': dogyun_config['DOGYUN_CSRF_TOKEN'],
@@ -154,10 +164,11 @@ def draw_lottery(message):
     }
     # 发送put请求
     try:
-        response = requests.put(url, headers=headers,verify=True)
+        response = requests.put(url, headers=headers, verify=True)
         if response.url == 'https://account.dogyun.com/login':
             # tg通知dogyun cookie已过期
-            bot.send_message(dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
+            bot.send_message(
+                dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
             return
         data = response.json()
     except Exception as e:
@@ -168,14 +179,16 @@ def draw_lottery(message):
     if result:
         # 查看奖品
         prize_url = f'https://console.dogyun.com/turntable/prize/page'
-        prize_body = {"draw":2,"columns":[{"data":"prizeName","name":"","searchable":True,"orderable":False,"search":{"value":"","regex":False}},
-                                          {"data":"status","name":"","searchable":True,"orderable":False,"search":{"value":"","regex":False}},
-                                          {"data":"createTime","name":"","searchable":True,"orderable":True,"search":{"value":"","regex":False}},
-                                          {"data":"descr","name":"","searchable":True,"orderable":False,"search":{"value":"","regex":False}}]
-                                          ,"order":[{"column":2,"dir":"desc"}],"start":0,"length":10,"search":{"value":"","regex":False}}
+        prize_body = {"draw": 2, "columns": [{"data": "prizeName", "name": "", "searchable": True, "orderable": False, "search": {"value": "", "regex": False}},
+                                             {"data": "status", "name": "", "searchable": True, "orderable": False, "search": {
+                                                 "value": "", "regex": False}},
+                                             {"data": "createTime", "name": "", "searchable": True, "orderable": True, "search": {
+                                                 "value": "", "regex": False}},
+                                             {"data": "descr", "name": "", "searchable": True, "orderable": False, "search": {"value": "", "regex": False}}], "order": [{"column": 2, "dir": "desc"}], "start": 0, "length": 10, "search": {"value": "", "regex": False}}
         # post请求
         try:
-            prize_response = requests.post(prize_url,json = prize_body ,headers=headers,verify=True)
+            prize_response = requests.post(
+                prize_url, json=prize_body, headers=headers, verify=True)
         except Exception as e:
             bot.reply_to(message, f'查看奖品失败: {e.args[0]}')
             return
@@ -187,49 +200,54 @@ def draw_lottery(message):
             bot.reply_to(message, 'dogyun cookie已过期,请更新cookie')
             return
         # 获取奖品信息
-        prize_infos:list = prize_data['data']
-        
+        prize_infos: list = prize_data['data']
+
         if len(prize_infos) > 0 and prize_infos[0]['createTime'].split(' ')[0] == date.today().strftime("%Y-%m-%d"):
-            bot.reply_to(message, f'抽奖结果: 成功\n奖品: {prize_infos[0]["prizeName"]}\n状态: {prize_infos[0]["status"]}\n描述: {prize_infos[0]["descr"]}')
+            bot.reply_to(
+                message, f'抽奖结果: 成功\n奖品: {prize_infos[0]["prizeName"]}\n状态: {prize_infos[0]["status"]}\n描述: {prize_infos[0]["descr"]}')
     else:
         bot.reply_to(message, f'抽奖失败: {data["message"]}')
-        
+
+
 @bot.message_handler(commands=['update_xray_route'])
 def update_xray_route(message):
     """更新xray客户端路由规则
 
     Args:
         message (_type_): _description_
-    """    
+    """
     script = 'curl -s https://raw.githubusercontent.com/nichuanfang/domestic-rules-generator/main/crontab.sh | bash'
     try:
-        ssd_fd = ssh_connect('154.202.60.190',60022,'root','Ld08MAiSoL8Ag9P')
+        ssd_fd = ssh_connect('154.202.60.190', 60022,
+                             'root', 'Ld08MAiSoL8Ag9P')
     except:
         bot.reply_to(message, '无法连接到服务器154.202.60.190')
         return
     try:
-        ssh_exec_cmd(ssd_fd,script)
+        ssh_exec_cmd(ssd_fd, script)
     except:
         bot.reply_to(message, '执行脚本报错')
         return
     ssh_close(ssd_fd)
     bot.reply_to(message, '更新xray客户端路由规则成功')
-        
+
+
 @bot.message_handler(commands=['bitwarden_backup'])
 def bitwarden_backup(message):
     """备份bitwarden
 
     Args:
         message (_type_): _description_
-    """    
+    """
     script = 'curl -s https://raw.githubusercontent.com/nichuanfang/config-server/master/linux/bash/step2/vps/backup_bitwarden.sh | bash'
     try:
-        ssd_fd = ssh_connect('154.202.60.190',60022,'root','Ld08MAiSoL8Ag9P')
+        ssd_fd = ssh_connect('154.202.60.190', 60022,
+                             'root', 'Ld08MAiSoL8Ag9P')
     except:
         bot.reply_to(message, '无法连接到服务器154.202.60.190')
         return
     try:
-        ssh_exec_cmd(ssd_fd,script)
+        ssh_exec_cmd(ssd_fd, script)
     except:
         bot.reply_to(message, '执行脚本报错')
         return
@@ -237,24 +255,27 @@ def bitwarden_backup(message):
     bot.reply_to(message, '备份bitwarden成功')
 
 # 执行bash脚本
+
+
 @bot.message_handler(commands=['exec_cmd'])
 def exec_cmd(message):
     """执行bash脚本
 
     Args:
         message (_type_): _description_
-    """    
+    """
     script = message.text[10:]
-    if script in ['systemctl stop tgbot','systemctl restart tgbot','reboot']:
+    if script in ['systemctl stop tgbot', 'systemctl restart tgbot', 'reboot']:
         bot.reply_to(message, '禁止执行该命令')
         return
     try:
-        ssd_fd = ssh_connect('154.202.60.190',60022,'root','Ld08MAiSoL8Ag9P')
+        ssd_fd = ssh_connect('154.202.60.190', 60022,
+                             'root', 'Ld08MAiSoL8Ag9P')
     except:
         bot.reply_to(message, '无法连接到服务器154.202.60.190')
         return
     try:
-        ssh_exec_cmd(ssd_fd,script)
+        ssh_exec_cmd(ssd_fd, script)
     except:
         bot.reply_to(message, '执行命令报错')
         return
@@ -265,7 +286,7 @@ def exec_cmd(message):
 # 每月7号
 def get_traffic_packet():
     """自动领取流量包
-    """    
+    """
     url = f'https://cvm.dogyun.com/traffic/package/level'
     headers = {
         'X-Csrf-Token': dogyun_config['DOGYUN_CSRF_TOKEN'],
@@ -275,10 +296,11 @@ def get_traffic_packet():
     }
     try:
         # 发送post请求
-        response = requests.post(url, headers=headers,verify=True)
+        response = requests.post(url, headers=headers, verify=True)
         if response.url == 'https://account.dogyun.com/login':
             # tg通知dogyun cookie已过期
-            bot.send_message(dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
+            bot.send_message(
+                dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
             return
         data = response.json()
     except Exception as e:
@@ -300,9 +322,11 @@ def get_traffic_packet():
     bot.send_message(dogyun_config['CHAT_ID'], f'等级奖励通用流量包: {result}')
 
 # 每天获取通知
+
+
 def lucky_draw_notice():
     """抽奖活动通知
-    """ 
+    """
     url = f'https://console.dogyun.com/turntable'
     headers = {
         'Cookie': dogyun_config['DOGYUN_COOKIE'],
@@ -312,29 +336,30 @@ def lucky_draw_notice():
     }
     try:
         # 发起get请求
-        response = requests.get(url, headers=headers,verify=True)
+        response = requests.get(url, headers=headers, verify=True)
         if response.url == 'https://account.dogyun.com/login':
             # tg通知dogyun cookie已过期
-            bot.send_message(dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
+            bot.send_message(
+                dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
             return
-        data = response.json()
     except Exception as e:
         logger.error(e)
         return
     soup = BeautifulSoup(response.text, 'lxml')
     try:
-        result = soup.find('a',class_='gb-turntable-btn').text
-        bot.send_message(dogyun_config['CHAT_ID'], f'抽奖活动通知: {soup.find("strong").text}')
+        result = soup.find('a', class_='gb-turntable-btn').text
+        bot.send_message(dogyun_config['CHAT_ID'],
+                         f'抽奖活动通知: {soup.find("strong").text}')
         logger.info(f'抽奖活动通知: {soup.find("strong").text}')
     except:
         # '暂无抽奖活动'
         pass
-    
-    
+
+
 # 余额不足提醒
 def balance_lack_notice():
     """余额不足提醒
-    """ 
+    """
     url = f'https://console.dogyun.com'
     headers = {
         'Cookie': dogyun_config['DOGYUN_COOKIE'],
@@ -344,17 +369,18 @@ def balance_lack_notice():
     }
     try:
         # 发起get请求
-        response = requests.get(url, headers=headers,verify=True)
+        response = requests.get(url, headers=headers, verify=True)
         if response.url == 'https://account.dogyun.com/login':
             # tg通知dogyun cookie已过期
-            bot.send_message(dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
+            bot.send_message(
+                dogyun_config['CHAT_ID'], 'dogyun cookie已过期,请更新cookie! \n https://github.com/nichuanfang/tgbot/edit/main/settings/config.py')
             return
     except Exception as e:
         logger.error(e)
         return
     soup = BeautifulSoup(response.text, 'lxml')
     try:
-        result = soup.find('span',class_='h5 font-weight-normal').text
+        result = soup.find('span', class_='h5 font-weight-normal').text
         # 根据正则表达式提取数字
         balance = re.findall(r"\d+\.?\d*", result)[0]
         if float(balance) < 10:
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import telebot
 from bots import dogyun_bot
-from bots.dogyun_bot import get_traffic_packet,lucky_draw_notice,balance_lack_notice
+from bots.dogyun_bot import get_traffic_packet, lucky_draw_notice, balance_lack_notice
 from bots import github_workflow_bot
 from bots import tmdb_bot
 import logging
@@ -13,42 +13,49 @@ logger = telebot.logger
 telebot.logger.setLevel(logging.INFO)
 scheduler = BlockingScheduler()
 
+
 def dogyun_bot_func():
     dogyun_bot.bot.remove_webhook()
     # 启动轮询
     dogyun_bot.bot.infinity_polling()
-    
+
+
 def github_workflow_bot_func():
     github_workflow_bot.bot.remove_webhook()
     # 启动轮询
     github_workflow_bot.bot.infinity_polling()
-    
+
 
 def tmdb_bot_func():
     tmdb_bot.bot.remove_webhook()
     # 启动轮询
     tmdb_bot.bot.infinity_polling()
 
+
 def scheduler_func():
     # 每月7号获取流量包
-    scheduler.add_job(get_traffic_packet, 'cron', id='get_traffic_packet', month='*', day='7', hour='9', minute='0', second='0')
+    scheduler.add_job(get_traffic_packet, 'cron', id='get_traffic_packet',
+                      month='*', day='7', hour='9', minute='0', second='0')
     # 每天9点通知抽奖活动
-    scheduler.add_job(lucky_draw_notice, 'cron', id='lucky_draw_notice', month='*', day='*', hour='9', minute='0', second='0')
+    scheduler.add_job(lucky_draw_notice, 'cron', id='lucky_draw_notice',
+                      month='*', day='*', hour='23', minute='08', second='0')
     # 每天9点通知余额不足
-    scheduler.add_job(balance_lack_notice, 'cron', id='balance_lack_notice', month='*', day='*', hour='9', minute='0', second='0')
+    scheduler.add_job(balance_lack_notice, 'cron', id='balance_lack_notice',
+                      month='*', day='*', hour='9', minute='0', second='0')
     logger.info('Scheduler started!')
     # 开启定时任务
     scheduler.start()
+
 
 if __name__ == '__main__':
     thread1 = threading.Thread(target=dogyun_bot_func, daemon=True)
     thread2 = threading.Thread(target=github_workflow_bot_func, daemon=True)
     thread3 = threading.Thread(target=tmdb_bot_func, daemon=True)
     thread4 = threading.Thread(target=scheduler_func, daemon=True)
-    
+
     thread1.start()
     thread2.start()
     thread3.start()
     thread4.start()
-    
+
     thread1.join()
