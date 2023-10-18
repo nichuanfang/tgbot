7c16ccfd98475b6063dad9af60454d7e50bd261c
nichuanfang <f18326186224@gmail.com>
2023-10-19 01:23:50 +0800

    ✨ feat: 日期矫正

--- bots/train_bot.py
+++ bots/train_bot.py
@@ -516,7 +516,7 @@ def query_handler(message, stations, from_station, to_station):
         bot.register_next_step_handler(
             sent_msg, query_handler, stations, from_station, to_station)
         return None
-    elif (datetime.datetime.strptime(train_date, '%Y-%m-%d') - datetime.datetime.now()).days < 0:
+    elif datetime.datetime.strptime(train_date, '%Y-%m-%d').day - datetime.datetime.now().day < 0:
         text = '日期必须大于今天,请重新输入'
         sent_msg = bot.send_message(message.chat.id, text)
         bot.register_next_step_handler(
