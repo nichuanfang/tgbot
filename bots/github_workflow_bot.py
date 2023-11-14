72a9f7fcfca92944ab70d3c27fbf4813ce1d329e
nichuanfang <f18326186224@gmail.com>
2023-11-14 17:54:38 +0800

    ✨ feat: 新增bark通知

--- bots/github_workflow_bot.py
+++ bots/github_workflow_bot.py
@@ -38,36 +38,39 @@ def update_proxy(message):
     }
     requests.post('https://api.github.com/repos/nichuanfang/nogfw/dispatches',
                   data=json.dumps({"event_type": "更新备用代理池"}), headers=header)
-    bot.reply_to(message, '已触发工作流: 更新备用代理池')
+    bot.reply_to(
+        message, '已触发工作流: 更新备用代理池,查看工作流日志: https://github.com/nichuanfang/nogfw/actions')
 
 
-@bot.message_handler(commands=['generate_girlfriend_chat'])
-def generate_girlfriend_chat(message):
-    """生成每日女友问候
+# @bot.message_handler(commands=['generate_girlfriend_chat'])
+# def generate_girlfriend_chat(message):
+#     """生成每日女友问候
 
-    Returns:
-        _type_: _description_
-    """
-    header = {
-        'Accept': 'application/vnd.github.everest-preview+json',
-        'Authorization': f'token {github_config["GITHUB_TOKEN"]}'
-    }
-    requests.post('https://api.github.com/repos/nichuanfang/wechat-girlfriend-push/dispatches',
-                  data=json.dumps({"event_type": "生成每日女友问候"}), headers=header)
-    bot.reply_to(message, '已触发工作流: 生成每日女友问候')
+#     Returns:
+#         _type_: _description_
+#     """
+#     header = {
+#         'Accept': 'application/vnd.github.everest-preview+json',
+#         'Authorization': f'token {github_config["GITHUB_TOKEN"]}'
+#     }
+#     requests.post('https://api.github.com/repos/nichuanfang/wechat-girlfriend-push/dispatches',
+#                   data=json.dumps({"event_type": "生成每日女友问候"}), headers=header)
+#     bot.reply_to(
+#         message, '已触发工作流: 生成每日女友问候,查看工作流日志: https://github.com/nichuanfang/wechat-girlfriend-push/actions')
 
 
-@bot.message_handler(commands=['update_everyday_wallpaper'])
-def update_everyday_wallpaper(message):
-    """ 更新每日壁纸
+# @bot.message_handler(commands=['update_everyday_wallpaper'])
+# def update_everyday_wallpaper(message):
+#     """ 更新每日壁纸
 
-    Returns:
-        _type_: _description_
-    """
-    header = {
-        'Accept': 'application/vnd.github.everest-preview+json',
-        'Authorization': f'token {github_config["GITHUB_TOKEN"]}'
-    }
-    requests.post('https://api.github.com/repos/nichuanfang/nichuanfang/dispatches',
-                  data=json.dumps({"event_type": " 更新每日壁纸"}), headers=header)
-    bot.reply_to(message, '已触发工作流: 更新每日壁纸')
+#     Returns:
+#         _type_: _description_
+#     """
+#     header = {
+#         'Accept': 'application/vnd.github.everest-preview+json',
+#         'Authorization': f'token {github_config["GITHUB_TOKEN"]}'
+#     }
+#     requests.post('https://api.github.com/repos/nichuanfang/nichuanfang/dispatches',
+#                   data=json.dumps({"event_type": " 更新每日壁纸"}), headers=header)
+#     bot.reply_to(
+#         message, '已触发工作流: 更新每日壁纸,查看工作流日志: https://github.com/nichuanfang/nichuanfang/actions')
